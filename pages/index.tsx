d0147e73f245f964448119fe3089ce84df218159
Karan Kashyap <karankashyap@gmail.com>
2023-09-08 19:44:04 -0400

    make changes for design bootcamp

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,34 +7,19 @@ import LinkCard from "../components/LinkCard";
 
 const workshops: { name: string; description: string; image: string; handout: string; slides?: string; recording?: string; }[] = [
     {
-        name: "Day 1: Web Fundamentals",
+        name: "Day 1: TBD",
         description: "Start with the underlying technologies of the web: HTML, CSS, and Javascript.",
         image: "/basics.png",
-        handout: "https://docs.google.com/document/d/1c3VRt4PgSsJcJjQnO3ybej8tHbJF0gYu0Ydf-gZXvRw/edit?usp=sharing",
-        slides: "https://docs.google.com/presentation/d/1G2giTCFRvywLJ6Y0yccIgiDK3qyJx7T_mP6D4LH0TCA/edit?usp=sharing",
+        handout: "",
+        slides: "",
         // recording: "https://brown.zoom.us/rec/share/uGXXyTsPD0g1Iq9B9Io7JClsrlD00Tf98ELdG6f5jrAyNPtqz5fV7oF8W3kQtfnu.RZhZiPMoNOeA1vp1",
     }, {
-        name: "Day 2: Express + MongoDB",
+        name: "Day 2: TBD",
         description: "Build a server to persist and manipulate data for your web apps using Express and MongoDB.",
         image: "/backend.png",
-        handout: "https://docs.google.com/document/d/1EYAvslfccZSvYkq_vKsWODYHLydxtU8PW0fCtJoAGMc/edit?usp=sharing",
-        slides: "https://docs.google.com/presentation/d/1hF3TuzTu6exQPA9ekFjQdP-OPg-E0-YeU5gUDayoLYw/edit?usp=sharing",
+        handout: "",
+        slides: "",
         // recording: "",
-    },
-    {
-        name: "Day 3: React",
-        description: "Build a user interface for your app using React: a modern front-end framework.",
-        image: "/react.png",
-        handout: "https://docs.google.com/document/d/1GuqxjAZ6mkzfLBbbJC14q0bCK3TRweEZS5lVXKzUS1E/edit?usp=sharing",
-        slides: "https://docs.google.com/presentation/d/1gDeKXtuXb33L6nT3cWZVKKxH3BrxFeU3W7zfLYHpbEY/edit?usp=sharing",
-        // recording: "https://brown.zoom.us/rec/share/Jy5c8U1Nh03YE_LD3ma3aDRd0SPE7MMdufCQUwb8NEip8-8YpFKGnuFIxcpmjCRw.wX9CGRDM04rhLouH",
-    }, {
-        name: "Day 4: Integration",
-        description: "Connect your front-end with your server with API calls, then deploy your app to the web.",
-        image: "/deploy.png",
-        handout: "https://docs.google.com/document/d/1Hld8akpY6k5v2KeWvEoL1uS9mQZourflHn7bEljujfY/edit?usp=sharing",
-        slides: "https://docs.google.com/presentation/d/14RujbxgLKe6B06LPbI8jnSg5vpRmPuF67GAg_arPCWo/edit?usp=sharing",
-        // recording: "https://brown.zoom.us/rec/share/zM88u1r0WRDvX6nPybnlYVC7EWZ1R5IZtlyutVfb5IScPmmnD-gNxGWRK8UGQPU.96_hU7bwzUYP-uia",
     }];
 
 const Home: NextPage = () => {
@@ -47,7 +32,7 @@ const Home: NextPage = () => {
 
     return (<div>
         <Head>
-          <title>FSAB Bootcamp Fall 2023</title>
+          <title>FSAB Design Bootcamp Fall 2023</title>
           <meta property="og:title" content="FSAB Bootcamp Fall 2023" key="title" />
         </Head>
         <div
@@ -57,30 +42,31 @@ const Home: NextPage = () => {
                     className={`p-4 z-50 text-sm sm:text-base flex items-center justify-between fixed bg-opacity-40 transition-all duration-150 bg-[#06080D] backdrop-blur-lg w-full ${scrollPosition < 10 && "backdrop-blur-none bg-opacity-0"}`}>
                     <p className="font-bold">Full Stack at Brown</p>
                     <nav className="space-x-6">
-                        <a href="https://calendar.google.com/calendar/u/0?cid=Y181NGI4ODA2NmE3N2U0MTdiODZiOTQ3NzVjZjhmOWFjMDZjYTgxMzJlNzBmMWJmYzcxODUxODRhZjNmYzJhYjU3QGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20">Add
-                            to Calendar</a>
+                        {/* <a href="https://calendar.google.com/calendar/u/0?cid=Y181NGI4ODA2NmE3N2U0MTdiODZiOTQ3NzVjZjhmOWFjMDZjYTgxMzJlNzBmMWJmYzcxODUxODRhZjNmYzJhYjU3QGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20">Add
+                            to Calendar</a> */}
+                            <p>Add to calendar (TODO: Fix)</p>
                         {/* <a href="https://www.fullstackatbrown.com/about">About Us</a> */}
                     </nav>
                 </div>
                 <div className="max-w-5xl px-4 mx-auto pt-36 space-y-24">
                     <section>
                         <p className="text-lg md:text-xl font-mono uppercase">
-                            Sep 11 — 14, 2023
+                            Sep 11 — 12, 2023
                         </p>
                         <h1 className="mt-2 text-4xl md:text-6xl font-semibold">
-                            Web Development Bootcamp
+                            Web Design Bootcamp
                         </h1>
                         <p className="text-xl md:text-2xl opacity-75 font-light mt-4">
                             Meet other Full Stack at Brown members and enjoy a light snack on us, all while learning modern web
-                            development.
-                            Throughout the week, you&apos;ll learn industry-standard tooling and technologies by
-                            building a real project you can add to your portfolio.
+                            design.
+                            Throughout the bootcamp, you&pos;ll learn how to use the tools needed to design amazing
+                            websites.
                         </p>
                         <p className="text-l md:text-xl opacity-75 mt-4">
-                            Times: <strong>September 11 - 14, 8pm - 10pm</strong>
+                            Times: <strong>September 11 - 12, 8pm - 10pm</strong>
                         </p>
                         <p className="text-l md:text-xl opacity-75 mt-1">
-                            Location: <strong>Barus & Holley 166</strong>
+                            Location: <strong>Barus & Holley 141</strong>
                         </p>
                         {/* <p className="text-l md:text-xl opacity-75 mt-1">
                             Zoom Link: <a className="underline" href="https://brown.zoom.us/j/96870814005">https://brown.zoom.us/j/96870814005</a>
@@ -119,15 +105,15 @@ const Home: NextPage = () => {
                         More stuff
                     </h2>
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
-                        <LinkCard title="Day 0: Set up your development environment"
-                                href="https://docs.google.com/document/d/1olSoQayXFkPtc6a_v05fCKcyx-R1kZ69TndzWP9w7uo/edit?usp=sharing"
-                                  description="Download and install the tooling we'll use throughout the week."/>
+                        <LinkCard title="Day 0: Setup???"
+                                href=""
+                                  description="Download and install the tools we'll use throughout the week."/>
                         {/*<LinkCard title="Download project starter"*/}
                         {/*          description="Clone the starter code for the project we'll build together."/>*/}
                         {/*<LinkCard title="View project demo"*/}
                         {/*          description="See a live demo of the project you'll build throughout the week."/>*/}
                         <LinkCard title="Submit your project for certification"
-                                  href="https://forms.gle/pVP2G9pDBqaP2jNH9"
+                                  href="???"
                                   description="Send us your completed bootcamp project to become eligible to work on projects for
                                     Full Stack."/>
                     </div>
